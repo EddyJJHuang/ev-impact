72cc29263a3caafe5f05030a696058f3dde0a9d6
Eddy Huang <huangjiajun113@gmail.com>
2023-09-04 15:13:46 -0700

    add another dot on battery

--- src/CarModel.js
+++ src/CarModel.js
@@ -8,12 +8,21 @@ function CarModel() {
     <div className="car-container">
       <img src="/car.png" alt="Car" />
       <button 
-        className="dot-button" 
-        onClick={() => setShowDescription(!showDescription)}
+        className="dot-button dot1" 
+        onClick={() => setShowDescription(showDescription === 'dot1' ? null : 'dot1')}
       ></button>
-      {showDescription && (
-        <div className="description-box">
-          This is description of the tire.
+      <button 
+        className="dot-button dot2" 
+        onClick={() => setShowDescription(showDescription === 'dot2' ? null : 'dot2')}
+      ></button>
+      {showDescription === 'dot1' && (
+        <div className="description-box dot1">
+          This is description of tire
+        </div>
+      )}
+      {showDescription === 'dot2' && (
+        <div className="description-box dot2">
+          This is description of battery
         </div>
       )}
       <img src="/battery.png" alt="Electric Car Battery" className="battery-image" />
